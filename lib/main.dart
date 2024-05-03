867c894afaf2c890b0d2e03a2ac37159af4fd386
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-05-03 20:39:10 +0900

    feat: Create UI with title, text form and button.

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,44 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: "Hello World",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Hello World",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(hintText: "Title"),
+              ),
+            ),
+            TextButton(
+              onPressed: null,
+              child: const Text("Change"),
+            )
+          ],
         ),
       ),
     );
